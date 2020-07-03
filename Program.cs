1fab69c0eb7483107258198a594849ab5dc8e25f
Che Sergiu <37384588+CHESERGIU@users.noreply.github.com>
2020-07-03 09:36:51 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace App6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hello = "Hello";
+            string world = "world";
+            string message = hello + " ";
+            message += world + "!";
+            Console.WriteLine(message);
+            
+            
+            Console.Read();
+        }
+    }
+}
